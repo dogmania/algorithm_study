32137efaa7cadff5124d635ca1f1202762e99f52
dogmania <hj296914@gmail.com>
2024-07-01 15:04:21 +0900

    8 - 2 문제 구현

--- ch8/ch8.cpp
+++ ch8/ch8.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cmath>
-#include <climits>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <algorithm>
+// #include <cmath>
+// #include <climits>
 
-#define ll long long
+// #define ll long long
 
-using namespace std;
+// using namespace std;
 
-vector<ll> d(100, 0);
+// vector<ll> d(100, 0);
 
-ll fibo(ll x) {
-    if (x == 1 || x == 2) return 1;
+// ll fibo(ll x) {
+//     if (x == 1 || x == 2) return 1;
 
-    if (d[x] != 0) return d[x];
+//     if (d[x] != 0) return d[x];
 
-    d[x] = fibo(x - 1) + fibo(x - 2);
-    return d[x];
-}
+//     d[x] = fibo(x - 1) + fibo(x - 2);
+//     return d[x];
+// }
  
-int main() {
-    cout << fibo(50) << '\n';
-}
+// int main() {
+//     cout << fibo(50) << '\n';
+// }
