00d4619a58eb6152085dc4e791e251aa42e75519
dogmania <hj296914@gmail.com>
2024-07-01 14:11:26 +0900

    dp 개념 학습

---
+++ ch8/ch8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define ll long long
+
+using namespace std;
+
+vector<ll> d(100, 0);
+
+ll fibo(ll x) {
+    if (x == 1 || x == 2) return 1;
+
+    if (d[x] != 0) return d[x];
+
+    d[x] = fibo(x - 1) + fibo(x - 2);
+    return d[x];
+}
+ 
+int main() {
+    cout << fibo(50) << '\n';
+}
