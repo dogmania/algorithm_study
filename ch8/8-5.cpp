b3fa9f39aefed33e4ac8ab9438b2d2c9f7874293
dogmania <hj296914@gmail.com>
2024-07-19 14:15:07 +0900

    주석 처리

--- ch8/8-5.cpp
+++ ch8/8-5.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cmath>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <algorithm>
+// #include <cmath>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> coin(n, 0);
-    vector<int> dp(m + 1, 10001);
+// int main() {
+//     int n, m;
+//     cin >> n >> m;
+//     vector<int> coin(n, 0);
+//     vector<int> dp(m + 1, 10001);
 
-    dp[0] = 0;
+//     dp[0] = 0;
 
-    for (int i = 0; i < n; i++) {
-        cin >> coin[i];
-        dp[coin[i]] = 1;
-    }
+//     for (int i = 0; i < n; i++) {
+//         cin >> coin[i];
+//         dp[coin[i]] = 1;
+//     }
 
-    for(int i = 0; i < n; i++) {
-        for (int j = coin[i]; j < m + 1; j++) {
-            dp[j] = min(dp[j], dp[j - coin[i]] + 1);
-        }
-    }
+//     for(int i = 0; i < n; i++) {
+//         for (int j = coin[i]; j < m + 1; j++) {
+//             dp[j] = min(dp[j], dp[j - coin[i]] + 1);
+//         }
+//     }
 
-    if (dp[m] > 10000) cout << -1 << '\n';
-    else cout << dp[m] << '\n';
-}
+//     if (dp[m] > 10000) cout << -1 << '\n';
+//     else cout << dp[m] << '\n';
+// }
